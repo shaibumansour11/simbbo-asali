2b762f565b11c5cbdc550b8a91760427a819be62
shaibu <shaibumansour10@gmail.com>
2025-08-28 15:30:51 +0300

    Updated file: page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,15 @@ import { Hero } from "@/components/hero"
 import { MissionSection } from "@/components/mission-section"
 import { FeaturesGrid } from "@/components/features-grid"
 import { PageTransition } from "@/components/page-transition"
+import ProductsPage from "./products/page"
+import MediaPage from "./media/page"
 
 export default function HomePage() {
   return (
     <PageTransition>
       <Hero />
+      <ProductsPage/>
+      < MediaPage/>
       <MissionSection />
       <FeaturesGrid />
     </PageTransition>
